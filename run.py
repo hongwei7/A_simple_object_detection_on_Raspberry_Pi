0a3850db2262d470d390467d20343b7cd1552a26
hongwei7 <1216869567@qq.com>
2019-08-01 15:01:17 +0800

    程序优化

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 import numpy as np
 import re
 import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import time
 #将类别ID转换为标签
 def load_index(model_dir):
@@ -27,6 +28,8 @@ def create_graph(model_dir):
     tf.import_graph_def(graph_def, name='')
 
 def main():
+  print('需要几次检测(第一次检查需要较长时间):')
+  k=int(input())
   #目录设置
   model_dir='/home/pi/Desktop/object_detection_on_pi'
   image_path='/home/pi/Desktop'
@@ -38,32 +41,33 @@ def main():
   create_graph(model_dir)
   sess=tf.Session()
   softmax_tensor= sess.graph.get_tensor_by_name('softmax:0')
-  #开始预测
-  print('需要几次检测(第一次检查需要较长时间):')
-  k=int(input())
+  #开始预测 
   j=0
   while(j<k):
     print('第%d次检测!'%(j+1))
-    os.system("raspistill -o /home/pi/Desktop/keychain.jpg -t 1")
+    os.system("raspistill -o /home/pi/Desktop/keychain.jpg -t 500")
     result=[]
     for image in images:
       if image[-3:] in ['jpg','JPG','peg','PEG']:
         image_data = tf.gfile.FastGFile(image_path+'/'+image, 'rb').read()
         #输入图像数据，得到softmax概率值（一个shape=(1,1008)的向量）
+        print('已拍摄')
         predictions = sess.run(softmax_tensor,{'DecodeJpeg/contents:0': image_data})
         #(1,1008)->(1008,)
         predictions = np.squeeze(predictions)
         objects=''
         split=','
-        for i in range(5):
-          if i==4:
+        max_predict_num=3
+        for i in range(max_predict_num):
+          if i==max_predict_num-1:
             split=' 或者 '
           objects=objects+split+index_dict[np.where(predictions==max(predictions))[0][0]]+' '
           predictions[predictions==max(predictions)]=0
         result.append(image+'中可能有 '+objects[1:-1]+'.')
-    print(result)
+    for yi in result:
+        print(yi)
     j+=1
-    #time.sleep(0.5)
+    time.sleep(1.5)
   print('退出.')
   sess.close()
   
