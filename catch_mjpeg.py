84fcd8a6c26ef810333f83a59fa854eb7bad78cd
hongwei7 <1216869567@qq.com>
2019-08-01 23:14:48 +0800

    优化抓取过程

--- catch_mjpeg.py
+++ catch_mjpeg.py
@@ -9,7 +9,7 @@ def get_host_ip():
     finally:
         s.close()
 
-    return '192.16.9.101'
+    return ip
 def catch_pic():
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36'}
